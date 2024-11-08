58a53fbbda0387aa97d6d5616297c8e8db95c787
Henri <henri@gmail.com>
2024-11-08 18:19:41 -0300

    Alterando o navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Navbar from './Layout/Navbar';
+import Home from './pages/Home'
+import Cultos from './pages/Cultos'
+import Escala from './pages/Escalas'
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    return (
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route exact path='/' element={<Home/>} />
+        <Route path='/cultos' element={<Cultos/>} />
+        <Route path='/escala' element={<Escala/>}/>
+    </Routes>
+      </BrowserRouter>
+    );
+  }
+  export default App;
